17ca926e2e6931f4c9fb1f7a99d9702ba01289fb
Rahul Chhabra <chhabrarahul2707@gmail.com>
2024-07-21 15:51:33 +0530

    Initial commit

--- main.go
+++ main.go
@@ -1,12 +1,1 @@
-package main
-import "fmt"
-// import "mymodule/calculatorpackage"
-
-func main() {
-    fmt.Println("Hello, World!")
-    // fmt.Println("Addition: ", calculatorpackage.Add(10, 5))
-    // fmt.Println("Subtraction: ", calculatorpackage.Subtract(10, 5))
-    // fmt.Println("Multiplication: ", calculatorpackage.Multiply(10, 5))
-   // Caller()
-    GoRoutines()
-}
+package main;
